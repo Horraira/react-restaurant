a85ed4ad19192b0bb365c841c01277c6a2a5e54e
AbuSohan <sohan@inflexionpointbd.com>
2022-08-01 09:09:17 +0600

    Create Menu Component

---
+++ src/components/body/MenuItem.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuItem = (props) => {
+  return (
+    <div>
+      <h1>{props.dish[0]}</h1>
+    </div>
+  );
+};
+
+export default MenuItem;
---
+++ src/data/dishes.js
@@ -0,0 +1,200 @@
+export const DISHES = [
+  {
+    id: 0,
+    name: "Pizza",
+    image: "assests/images/pizza.png",
+    category: "mains",
+    label: "Hot",
+    price: "4.99",
+    featured: true,
+    description:
+      "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Sohan",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my love to eat it!",
+        author: "Shamim",
+        date: "2017-9-16T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Mahadi",
+        date: "2019-02-16T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Rahat",
+        date: "2017-12-16T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Zahid",
+        date: "2019-12-02T17:57:28.556094Z",
+      },
+    ],
+  },
+  {
+    id: 1,
+    name: "Pasta",
+    image: "/assests/images/pasta.jpg",
+    category: "appetizer",
+    label: "",
+    price: "1.99",
+    featured: false,
+    description:
+      "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Sohan",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my love to eat it!",
+        author: "Shamim",
+        date: "2017-9-16T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Mahadi",
+        date: "2019-02-16T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Rahat",
+        date: "2017-12-16T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Zahid",
+        date: "2019-12-02T17:57:28.556094Z",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Donut",
+    image: "/assests/images/donuts.jpg",
+    category: "appetizer",
+    label: "New",
+    price: "1.99",
+    featured: false,
+    description:
+      "A quintessential ConFusion experience, is it a vada or is it a donut?",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Sohan",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my love to eat it!",
+        author: "Shamim",
+        date: "2017-9-16T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Mahadi",
+        date: "2019-02-16T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Rahat",
+        date: "2017-12-16T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Zahid",
+        date: "2019-12-02T17:57:28.556094Z",
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Cheese Cake",
+    image: "/assests/images/cheeseCake.jpg",
+    category: "dessert",
+    label: "",
+    price: "2.99",
+    featured: false,
+    description:
+      "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
+    comments: [
+      {
+        id: 0,
+        rating: 5,
+        comment: "Imagine all the eatables, living in conFusion!",
+        author: "Sohan",
+        date: "2018-10-16T17:57:28.556094Z",
+      },
+      {
+        id: 1,
+        rating: 4,
+        comment:
+          "Sends anyone to heaven, I wish I could get my love to eat it!",
+        author: "Shamim",
+        date: "2017-9-16T17:57:28.556094Z",
+      },
+      {
+        id: 2,
+        rating: 3,
+        comment: "Eat it, just eat it!",
+        author: "Mahadi",
+        date: "2019-02-16T17:57:28.556094Z",
+      },
+      {
+        id: 3,
+        rating: 4,
+        comment: "Ultimate, Reaching for the stars!",
+        author: "Rahat",
+        date: "2017-12-16T17:57:28.556094Z",
+      },
+      {
+        id: 4,
+        rating: 2,
+        comment: "It's your birthday, we're gonna party!",
+        author: "Zahid",
+        date: "2019-12-02T17:57:28.556094Z",
+      },
+    ],
+  },
+];
+
+export default DISHES;
