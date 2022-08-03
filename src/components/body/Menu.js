46a9a5bce1beaaa8fb9336a85f8873deb0b8b594
AbuSohan <sohan@inflexionpointbd.com>
2022-08-04 01:31:18 +0600

    Dish Detail component added

--- src/components/body/Menu.js
+++ src/components/body/Menu.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
 import DISHES from "../../data/dishes";
 import MenuItem from "./MenuItem";
+import DishDetail from "./DishDetail";
 
 export default class Menu extends Component {
   state = {
     dishes: DISHES,
+    selectedDish: null,
   };
+
+  onDishSelect = (dish) => {
+    this.setState({
+      selectedDish: dish,
+    });
+  };
+
   render() {
     const menu = this.state.dishes.map((item) => {
-      return <MenuItem dish={item} key={item.id} />;
+      return (
+        <MenuItem dish={item} key={item.id} onDishSelect={this.onDishSelect} />
+      );
     });
+
+    let dishDetail = null;
+    if (this.state.selectedDish != null) {
+      dishDetail = <DishDetail dish={this.state.selectedDish} />;
+    }
+
     return (
       <div className="container">
         <div className="row">
           <div className="col-6">{menu}</div>
+          <div className="col-6">{dishDetail}</div>
         </div>
       </div>
     );
