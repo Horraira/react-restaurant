a0dd72c31cb980fb2d751dd06afe924fdab41ebc
AbuSohan <sohan@inflexionpointbd.com>
2022-08-07 19:49:42 +0600

    preparing project for Router

--- src/components/body/Body.js
+++ src/components/body/Body.js
@@ -1,12 +1,11 @@
 import React from "react";
 import Menu from "./Menu";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
 
 const Body = () => {
-  return (
-    <div>
-      <Menu />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Body;
