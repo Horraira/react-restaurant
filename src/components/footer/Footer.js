006d131947806a943215910e58604ad521b7826c
AbuSohan <sohan@inflexionpointbd.com>
2022-08-12 12:25:58 +0600

    add footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-auto">
+            <p>Restrnt &#169; copyright 2020</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
